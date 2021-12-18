319b6a2f421b8976d7227cf59485f80ff6224966
BruceWayne222 <77908276+BruceWayne222@users.noreply.github.com>
2021-12-18 11:32:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone https://github.com/BruceWayne222/Auto-Filter-2.git
-cd /Auto-Filter-2
+git clone -b main https://github.com/BruceWayne222/Auto-Filter-2.git /tgmoviebot
+cd /tgmoviebot
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
